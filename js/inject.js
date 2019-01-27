7cfbabd853082238b6403d2750806bb1c87208f4
Kai Chen <kaichen120@gmail.com>
2019-01-27 09:33:47 -0800

    working version

--- js/inject.js
+++ js/inject.js
@@ -3,8 +3,10 @@ function update() {
         function() {
             // console.log(this.href);
             console.log("hovering on", this);
+
             var that = this;
             if(this.getAttribute('data-requested') !== 'true') {
+                this.setAttribute('data-requested', "true");
                 console.log("data-requested", this.getAttribute('data-requested'));
                 $.ajax({
                     url: `https://summarize-services.herokuapp.com/summarize?url=${this.href}`,
@@ -12,11 +14,12 @@ function update() {
                     success: function (data) {
                         that.setAttribute('title', data.summary);
                         that.setAttribute('data-toggle', "tooltip");
-                        that.setAttribute('data-requested', "true");
                         console.log("tooltip set", that)
                     },
-                    error: function (xhr) {
-                        console.log('error', xhr);
+                error: function (xhr) {
+                    that.setAttribute('title', "There was an error, please try hovering over an article.");
+                    that.setAttribute('data-toggle', "tooltip");
+                    console.log('error', xhr);
                     }
                 });
             }
@@ -54,7 +57,7 @@ update();
 //
 // // Start observing the target node for configured mutations
 // observer.observe(targetNode, config);
-//
+
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     switch (request.type){
