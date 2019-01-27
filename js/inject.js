c1c432156beb37e58eae29ab8a7aad1f776f4c94
Kai Chen <kaichen120@gmail.com>
2019-01-26 21:57:36 -0800

    inject.js: Simplify on hover

--- js/inject.js
+++ js/inject.js
@@ -2,11 +2,7 @@
     console.log("something happened");
     $( "a" ).hover(
         function() {
-            $(this ).append( $( "<span> ***</span>" ));
-            $(this).attr("data-tippy","Tooltip");
-            tippy($(this));
-        }, function() {
-            $( this ).find( "span:last" ).remove();
+            tippy('a', {'content': 'Thog dont care'});
         }
     );
 
