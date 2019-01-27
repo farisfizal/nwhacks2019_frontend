855a1cd646b2f53ef7b4148be5749be015943efd
adilimtiaz <adilimtiaz96@gmail.com>
2019-01-26 19:34:03 -0800

    Heres the code

---
+++ js/content.js
@@ -0,0 +1,5 @@
+let images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+    images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
+console.log(JSON.stringify(images));
