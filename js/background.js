50175439455dc14ad74cbca95875bda1881176f2
Kai Chen <kaichen120@gmail.com>
2019-01-27 13:54:35 -0800

    Loosen required permissions and remove unrequired code

--- js/background.js
+++ js/background.js
@@ -9,32 +9,3 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     //     file: 'inject.js'
     // });
 });
-
-
-var searchSummarizeApi = function(word) {
-    var query = word.selectionText;
-    // alert(word.linkUrl);
-    $.ajax({
-        url: `https://summarize-services.herokuapp.com/summarize?url=${word.linkUrl}`,
-        method: "GET",
-        success: function(data) {
-            console.log(typeof(this));
-            // tippy('a', {'content': data.summary});
-            // alert(data.summary);
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {type: "openModal"});
-            });
-            console.log('success', data.summary);
-        },
-        error: function(xhr) {
-            console.log('error', xhr);
-        }
-    });
-};
-
-chrome.contextMenus.create({
-    id: "thog",
-    title: "Summarize this",
-    contexts: ["all"],
-    onclick: searchSummarizeApi
-});
