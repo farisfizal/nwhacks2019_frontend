2e32108a94900455ac0b7f5c08321cc0c8073cb2
Kai Chen <kaichen120@gmail.com>
2019-01-26 19:04:28 -0800

    background.js: Add non-working jquery script

--- js/background.js
+++ js/background.js
@@ -1,3 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
     // add an action here
 });
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    console.log("in chrome.browseraction.onclicked")
+    // for the current tab, inject the "inject.js" file & execute it
+    chrome.tabs.executeScript(tab.ib, {
+        file: 'inject.js'
+    });
+});
