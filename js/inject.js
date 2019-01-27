25337dbe8b8a1c67a3ba1e941e0fe588d1776127
Faris Fizal <farisfizal@gmail.com>
2019-01-26 22:06:49 -0800

    whack mutation stuff by faroos?

--- js/inject.js
+++ js/inject.js
@@ -1,4 +1,4 @@
-(function() {
+function update() {
     console.log("something happened");
     $( "a" ).hover(
         function() {
@@ -11,4 +11,27 @@
         $( this ).fadeIn( 500 );
     });
 
-})();
+};
+
+update();
+
+// Select the node that will be observed for mutations
+var targetNode = document.getElementById('a');
+
+// Options for the observer (which mutations to observe)
+var config = {
+    attributes: true,
+    childList: true,
+    subtree: true
+};
+
+// Callback function to execute when mutations are observed
+var callback = function (mutationsList, observer) {
+    update();
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
