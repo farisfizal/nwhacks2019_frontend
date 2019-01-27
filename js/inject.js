b2e56e0062b7f6fca634026fde222f2b94111f85
adilimtiaz <adilimtiaz96@gmail.com>
2019-01-27 00:35:05 -0800

    tried my best until tomoorow

--- js/inject.js
+++ js/inject.js
@@ -1,22 +1,33 @@
 function update() {
-    $( "a" ).hover(
-        function() {
-            $.ajax({
-                url: `https://summarize-service.herokuapp.com/summarize?url=${this.href}`,
-                method: "GET",
-                data: {
-                    a: "a"
-                },
-                success: function(data) {
-                    console.log('success', data)
-                },
-                error: function(xhr) {
-                    console.log('error', xhr);
+    $("a").hover(function(thisA)
+    {
+        let reqUrl = this.href;
+        console.log("reqUrl");
+        let api_key = "b6798d225e8f40e297dcdefaa6245175";
+        let api_url = "https://www.summarizebot.com/api/summarize";
+        axios.get(api_url, {
+            headers: {"Content-Type": "application/json"},
+            params: {
+                "apiKey": api_key,
+                "url": reqUrl,
+                "fragments": 15,
+                "language": "English",
+                "keywords": 10
+            }
+        })
+            .then(function (data) {
+                console.log(JSON.stringify(data));
+                let tooltipStr = "";
+                for(let i = 0; i<data.summary.length; i++){
+                    tooltipStr += data.summary[i].sentence + " ";
                 }
-            });
-            tippy('a', {'content': 'Thog dont care'});
-        }
-    );
+                console.log(tooltipStr);
+                tippy('a', {'content': tooltipStr});
+            })
+            .catch(function (error) {
+                console.log("I hate you");
+            })
+    });
 }
 
 update();
@@ -34,8 +45,8 @@ var config = {
 
 // Callback function to execute when mutations are observed
 var callback = function (mutationsList, observer) {
-    mutationsList.forEach(function(i) {
-        if(i.target.tagName === 'A') {
+    mutationsList.forEach(function (i) {
+        if (i.target.tagName === 'A') {
             console.log("anchor tag changed.");
             update();
         }
@@ -47,3 +58,14 @@ var observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
 observer.observe(targetNode, config);
+
+function waitABit() {
+    // Hackathon code
+    myVar = setTimeout(alertFunc, 3000);
+}
+
+function alertFunc() {
+    for (var i = 0; i < 100; i++) {
+        i++;
+    }
+}
